f954e54dbb46513da6b5c51a31d3ffef481574f2
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-10 17:59:32 +0530

    js

---
+++ string.js
@@ -0,0 +1,41 @@
+function str(){
+    //length 
+    let st = "this is a string "
+    // document.write("length is :",st.length);
+let st2 ="i am fromcybromOfficial"
+
+    //slice 
+    // document.write(st.slice(2,10))
+
+
+
+    //slice
+    // document.write(st2.slice(9,15))
+
+
+
+    //concate
+    // document.write(st.concat(st2));
+
+
+// console.log(st.trim())
+    //trim
+    // document.write(st)
+
+
+// uppercase
+    // document.write(st.toUpperCase());
+
+
+    //charAt
+    // document.write(st2.charAt(0));
+
+
+
+
+    
+}
+
+
+
+str ();
