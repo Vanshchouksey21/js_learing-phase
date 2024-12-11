e74994cf0a809cb383b5aa3cf11277e56ca2d846
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-11 19:19:00 +0530

    js

---
+++ arrays.js
@@ -0,0 +1,5 @@
+'use strict '
+//pop-delete the array from last 
+//push- increase the array from the last 
+//shift - delete the element from the first
+//unshift - increase the element from the first
