2e3ab39b239bb908e7bb51c453938a20f8506192
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-12 18:55:05 +0530

    all

--- arrays.js
+++ arrays.js
@@ -1,5 +1,58 @@
-'use strict '
+'use strict ';
 //pop-delete the array from last 
 //push- increase the array from the last 
 //shift - delete the element from the first
 //unshift - increase the element from the first
+// array define 
+
+let arr = [ " vansh " , "ansh" , 21 , 7];
+let arr2 = [ " yash " , "ansh" , 55 , 7];
+
+
+
+// for print 
+// document.write(arr);
+
+
+
+
+//for length of array
+// document.write(arr.length)
+
+
+
+// for concat the string
+// document.write(arr.concat(arr2));
+
+
+
+//for sorting the arraay in assendiing order
+let arr3=[ 2 ,3,1,44,33,2];
+// document.write(arr3.sort());
+
+
+
+
+
+
+// for reverse the array
+// document.write(arr3.reverse())
+
+
+
+
+
+// for desending order firstly sort the array and then reverse it ;
+// document.write(arr3.sort().reverse());
+
+
+//for
+
+
+
+
+function vans(){
+//for print the array in click 
+let arr = [ 2 ,3,1,44,33,2];
+document.write(arr.slice(2,5))
+}
