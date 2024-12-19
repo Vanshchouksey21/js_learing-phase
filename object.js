a655fdc086fabdbd6df2eba0fb6570c47a1d602d
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-19 19:07:50 +0530

    first commit

---
+++ object.js
@@ -0,0 +1,10 @@
+let std = {
+    name:"vansh",
+    age:19,
+    city:"bhopal"
+}
+console.log(std);
+console.log(std.age);
+std.mail="Vanshchouksey2175@mail.com"
+delete std.name;
+console.log(std)
