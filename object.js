e79529d622c79dd3f62adda01cf76bbc043373c2
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-19 19:34:38 +0530

    first commit

--- object.js
+++ object.js
@@ -1,10 +1,18 @@
 let std = {
     name:"vansh",
     age:19,
-    city:"bhopal"
+    city:"bhopal",
+    wlm: function(){
+        console.log(`welcome to india ${this.name}  `);
+       
+        
+    },
+    fruits:["apple","banana","mango"]
 }
+console.log(std.fruits[2])
 console.log(std);
 console.log(std.age);
 std.mail="Vanshchouksey2175@mail.com"
-delete std.name;
-console.log(std)
+// delete std.name;
+console.log(std);
+std.wlm();
