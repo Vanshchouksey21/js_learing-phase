a0549419ca9a444c7a63343ace423736053f2431
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-20 19:12:19 +0530

    first commit

--- object.js
+++ object.js
@@ -1,19 +1,31 @@
-let std = {
-    name:"vansh",
-    age:19,
-    city:"bhopal",
-    wlm: function(){
-        console.log(`welcome to india ${this.name}  `);
+// let std = {
+//     name:"vansh",
+//     age:19,
+//     city:"bhopal",
+//     wlm: function(){
+//         console.log(`welcome to india ${this.name}  `);
        
         
-    },
-    fruits:["apple","banana","mango"]
+//     },
+//     fruits:["apple","banana","mango"]
+// }
+let std1={
+    person:"one ",
+    from: "india"
 }
-console.log(std.fruits[2])
-console.log(std);
-console.log(std.age);
-std.mail="Vanshchouksey2175@mail.com"
-// delete std.name;
-console.log(std);
-std.wlm();
-// jsdbvcuj
+// console.log(std.fruits[2])
+// console.log(std);
+// console.log(std.age);
+// std.mail="Vanshchouksey2175@mail.com"
+// // delete std.name;
+// console.log(std);
+// std.wlm();
+// //destucture
+// let{age}=std;
+// console.log(age);
+
+
+// //  spread operator=basically it combines two array 
+// let comb = {...std ,...std1};
+// console.log(comb);
+
