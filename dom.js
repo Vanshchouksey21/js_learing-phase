95f790b2598ee90fdb92206115058ede0164d6b6
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-20 19:31:22 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,17 @@
+'use strict';
+let go=()=>{
+
+    // alert("alert")
+   let ans = document.querySelector("#vns")
+   ans.innerHTML="hello javascript"
+}
+
+
+
+let hhh=()=>{
+
+    
+   let ans2 = document.querySelector("#vns")
+   ans2.innerHTML="hello world"
+}
+
