90a985e9585b8d003fac30c9aa0221552aefdcd1
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-23 19:31:19 +0530

    first commit

--- dom.js
+++ dom.js
@@ -1,16 +1,60 @@
-'use strict';
-let go=()=>{
+// 'use strict';
+// let go=()=>{
 
-    // alert("alert")
-   let ans = document.querySelector("#vns")
-   ans.innerHTML="hello javascript"
-}
+//     // alert("alert")
+//    let ans = document.querySelector("#vns")
+//    ans.innerHTML="hello javascript"
+// }
 
 
 
-let hhh=()=>{
+// let hhh=()=>{
 
     
-   let ans2 = document.querySelector("#vns")
-   ans2.innerHTML="hello world";
+//    let ans2 = document.querySelector("#vns")
+//    ans2.innerHTML="hello world";
+// }
+
+
+
+let redclr=()=>{
+let box = document.querySelector("#box");
+box.style.backgroundColor="red"
 }
+let grnclr=()=>{
+   let box = document.querySelector("#box");
+   box.style.backgroundColor="green"
+   }
+   let blclr=()=>{
+      let box = document.querySelector("#box");
+      box.style.backgroundColor="blue"
+      }
+      let trnsclr=()=>{
+         let box = document.querySelector("#box");
+         box.style.backgroundColor="transparent"
+         }
+
+
+         let red=()=>{
+            let box1=document.querySelector("#box1");
+            box1.style.backgroundImage="url('/img/th.jpeg')";
+            box1.style.backgroundSize="cover";
+            box1.style.backgroundRepeat="no-repeat";
+        }
+        let blue=()=>{
+            let box1=document.querySelector("#box1");
+            box1.style.backgroundImage="url('/img/th (2).jpeg')";
+            box1.style.backgroundSize="cover";
+            box1.style.backgroundRepeat="no-repeat";
+        }
+        let green=()=>{
+            let box1=document.querySelector("#box1");
+            box1.style.backgroundImage="url('/img/th.jpeg')";
+            box1.style.backgroundSize="cover";
+            box1.style.backgroundRepeat="no-repeat";
+        }
+        let lv=()=>{
+         let box1=document.querySelector("#box1");
+         box1.style.backgroundColor="transparent"
+        
+     }
