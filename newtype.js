7482506de213a5d2230812c540a71c425fca9295
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2024-12-31 19:31:40 +0530

    calsi logo.

--- newtype.js
+++ newtype.js
@@ -1,9 +1,33 @@
 
-//IIFE- immediatly invoked function expression 
+// //IIFE- immediatly invoked function expression 
 
-( function(){
+// ( function(){
 
-    console.log("IIFE");
+//     console.log("IIFE");
+    
+// }
+// )();
+let off ;
+let counter=()=>{
+    var a = 10 ;
+     off = setInterval(() => {
+        
+        
+       
+
+        console.log(a);
+        a--;
+    }, 1000);
+
+   
     
 }
-)();
+let band=()=>{
+    clearInterval(off);
+}
+let isname=true;
+setInterval(()=>{
+    let display = document.querySelector("#vvv")
+    display.innerHTML=isname?"vansh":"chouksey"
+    isname= !isname
+},1000)
